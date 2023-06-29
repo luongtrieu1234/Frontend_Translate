fbd59e563fbebe8b7904169544ee8e3f82daff19
luongtrieu1234 <luongtrieu1234a@gmail.com>
2023-06-29 13:46:09 +0700

    add change button

--- src/App.js
+++ src/App.js
@@ -37,7 +37,22 @@ function Translation() {
             ></textarea>
           </div>
         </div>
-        <button onClick={translateText}>TRANSLATE</button>
+        <div className="text-note">
+          <div className='text-english'>
+            English
+          </div>
+          <div className="change">
+            <button className="button-change">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z" />
+              </svg>
+            </button>
+          </div>
+          <div className='text-vietnamese'>
+            Vietnamese
+          </div>
+        </div>
+        <button className="button-translate" onClick={translateText}>TRANSLATE</button>
       </div>
     </>
   );
